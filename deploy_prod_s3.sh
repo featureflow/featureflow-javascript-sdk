79c74fb4763e81532607efdf5c7f18f67dd05e54
Oliver Oldfield-Hodge <oliver.oldfield-hodge@industrieit.com>
2017-02-22 22:17:27 +1100

    DEploy featureflow.js to cdn.featueflow.io

---
+++ deploy_prod_s3.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#DEFAULT="default"
+#PROFILE=${AWS_PROFILE:-$DEFAULT}
+#BUCKET=allaboardapps-site-staging
+#DIR=./dist
+#aws s3 sync $DIR s3://$BUCKET/
+aws s3 cp dist/featureflow.js s3://cdn.featureflow.io/featureflow.js
+aws s3 cp dist/featureflow.min.js s3://cdn.featureflow.io/featureflow.min.js

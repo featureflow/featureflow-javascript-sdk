d07a00b27b964f3d304c0175b8d8382a00a78f1f
Henry Young <henry.young@industrieit.com>
2017-02-23 16:34:53 +1100

    Added versioning for featureflow.js cdn

--- deploy_prod_s3.sh
+++ deploy_prod_s3.sh
@@ -4,7 +4,15 @@
 #BUCKET=site-staging
 #DIR=./dist
 #aws s3 sync $DIR s3://$BUCKET/
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
 aws s3 cp dist/featureflow.js s3://cdn.featureflow.io/featureflow.js
 aws s3 cp dist/featureflow.min.js s3://cdn.featureflow.io/featureflow.min.js
-aws s3 cp dist/featureflow.js s3://controls.featureflow.io/featureflow.js
-aws s3 cp dist/featureflow.min.js s3://controls.featureflow.io/featureflow.min.js
+aws s3 cp dist/featureflow.js s3://cdn.featureflow.io/v$PACKAGE_VERSION/featureflow.js
+aws s3 cp dist/featureflow.min.js s3://cdn.featureflow.io/v$PACKAGE_VERSION/featureflow.min.js

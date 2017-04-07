18ac08353d31a68030a3acc10192811d3f0bfd32
Henry Young <henry.young@industrieit.com>
2017-04-07 11:19:13 +1000

    Whoops.

--- src/RestClient.js
+++ src/RestClient.js
@@ -7,7 +7,6 @@ type RequestConfig = {
 function request(endpoint: string, config: ConfigType, callback: NodeCallbackType<FeaturesType> = ()=>{}): XMLHttpRequest {
     let request = new XMLHttpRequest();
     request.addEventListener('load', function() {
-      console.log(request.status)
       if (request.status === 200 && request.getResponseHeader('Content-type') === "application/json;charset=UTF-8") {
         callback(null, JSON.parse(request.responseText));
       } else {
